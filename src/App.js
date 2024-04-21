83c7f934d04c0112f174c06948bf999c368a8246
Hemant Singh <Hemantsingh09103@gmail.com>
2024-04-22 01:45:12 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,9 @@ import Form from './modules/Form';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, auth=false }) => {
-  const isLoggedIn = localStorage.getItem('user:token') !== null || false;
 
-  if(!isLoggedIn && auth) {
-    return <Navigate to={'/users/sign_in'} />
-  }else if(isLoggedIn && ['/users/sign_in', '/users/sign_up'].includes(window.location.pathname)){
-    console.log('object :>> ');
     return <Navigate to={'/'} />
-  }
 
-  return children
 }
 
 function App() {
